120059b1157d9392eddf78a047ea3857c32ca3ec
Francesco Di Sales <f.disales@hotmail.com>
2022-12-04 01:21:49 +0100

    adding hack note table

---
+++ sql/hack_note.sql
@@ -0,0 +1,13 @@
+create table hack_note
+(
+
+        code char(4),
+        category varchar(500),
+        name varchar(200),
+        note serial,
+
+        primary key(code),
+        foreign key note references hack_voice(note)
+
+);
+          
